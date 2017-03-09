8b7e73159416fa6ae85a87ca850c9456fdd07b73
Lomeli12 <anthonylomeli11@gmail.com>
2017-03-08 20:01:14 -0800

    Slightly fancier compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,8 @@
-rm -rf build
+if [ -d "build" ]; then
+  echo "Removing build folder"
+  rm -rf build
+else
+  echo "Build folder does not exist, ignoring..."
+fi
+echo "Running gradle build..."
 gradle build
