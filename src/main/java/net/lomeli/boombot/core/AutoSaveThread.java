1d89b3b62a0342d51aac885246cce70939d939f2
Lomeli12 <anthonylomeli11@gmail.com>
2017-03-08 21:24:29 -0800

    Added AutoSave thread
    Will call a data write (and therefore, a DataWriteEvent) after a set interval. I've set the default value to 900000ms (15 minutes).

--- src/main/java/net/lomeli/boombot/core/AutoSaveThread.java
+++ src/main/java/net/lomeli/boombot/core/AutoSaveThread.java
@@ -1,4 +1,27 @@
 package net.lomeli.boombot.core;
 
-public class SaveThread {
+import net.lomeli.boombot.api.BoomAPI;
+import net.lomeli.boombot.api.nbt.NBTTagBase;
+import net.lomeli.boombot.lib.DataKeys;
+
+public class AutoSaveThread implements Runnable {
+    private long lastTime;
+    public static long SAVE_DELAY;
+
+    public AutoSaveThread() {
+        if (BoomAPI.dataRegistry.getBoomBotData().hasTag(DataKeys.AUTO_SAVE_DELAY, NBTTagBase.TagType.TAG_LONG))
+            SAVE_DELAY = BoomAPI.dataRegistry.getBoomBotData().getLong(DataKeys.AUTO_SAVE_DELAY);
+        else SAVE_DELAY = 900000L;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            long currentTime = System.currentTimeMillis();
+            if (currentTime >= lastTime + SAVE_DELAY) {
+                BoomAPI.dataRegistry.writeData();
+                lastTime = currentTime;
+            }
+        }
+    }
 }
